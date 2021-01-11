ebb2c627f340aecd5acf6d996f4e34b1904509ff
Kanisht Agarwal <55939035+kanisht09@users.noreply.github.com>
2021-01-11 16:32:09 +0530

    Update 0-1_Knapsack_Top_down.cpp

--- Dynamic Programming/C++/0-1_Knapsack_Top_down.cpp
+++ Dynamic Programming/C++/0-1_Knapsack_Top_down.cpp
@@ -6,38 +6,45 @@ using namespace std;
 
 #define ll long long
 
-
 // Problem Link :https://atcoder.jp/contests/dp/tasks/dp_d
-
 
 // Top down
 ll dp[102][100002];
 ll val[102];
 ll wt[102];
-ll n, W;
+ll n,W;
 
+ll knapsack(ll W,ll n)
+{
+    if(n==0 || W==0)
+    {
+        return 0;
+    }
+    if(dp[n][W]!=-1)
+    {
+        return dp[n][W];
+    }
+    if(wt[n-1]<=W)
+    {
+        return dp[n][W]=max(val[n-1]+knapsack(W-wt[n-1],n-1),knapsack(W,n-1));
+    }
+    return dp[n][W]=knapsack(W,n-1);
+    
+}
 
 int main()
 {
-
-#ifndef ONLINE_JUDGE
-    freopen("inp.txt", "r", stdin);
-    freopen("out.txt", "w", stdout);
-#endif
-
-    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-
-
-    cin >> n >> W;
-    for (ll i = 0; i < n; i++)
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    
+    memset(dp,-1,sizeof(dp));
+    cin>>n>>W;
+    for(ll i=0;i<n;i++)
     {
-        cin >> wt[i] >> val[i];
+        cin>>wt[i]>>val[i];
     }
-
-
-
-    cout << dp[n][W] << "\n";
-
+    knapsack(W,n);
+    cout<<dp[n][W]<<"\n";
+    
     return 0;
-
+    
 }
